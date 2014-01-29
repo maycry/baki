72da2a10d5206cd5979c7ab7f0d4cdc4262e2942
ura <byorickq@gmail.com>
2014-01-29 20:58:57 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 jQuery(document).ready(function($) {
+
+	/*-------languages-start-------*/
 	$(".language-current").click(function(event) {
 		event.stopPropagation();
+		$(this).toggleClass('active');
+		$(this).toggleClass('hover');
 		$(".language-popup").toggle();
+	});
+	$(".language-current").mouseover(function(event) {
+		if (!$(this).hasClass('active')) {
+			$(this).addClass('hover')
+		};
+	});
+	$(".language-current").mouseleave(function(event) {
+		if (!$(this).hasClass('active')) {
+			$(this).removeClass('hover')
+		};
 	});
 	$(".language-popup").click(function(event) {
 		event.stopPropagation();
@@ -9,9 +23,38 @@ jQuery(document).ready(function($) {
 	window.onkeyup = function (event) {
 		if (event.keyCode == 27) {
 			$(".language-popup").hide();
+			$(".language-current").removeClass('active');
 		}
 	}
 	$("html").click(function(event) {
 		$(".language-popup").hide();
+		$(".language-current").removeClass('active');
 	});
+	/*-------languages-end-------*/
+
+
+	/*-------tabs-start-------*/
+
+	$(".tabs a").click(function(event) {
+		$(".tabs a").removeClass('hover');
+		$(".tabs a").removeClass('active');
+		$(this).addClass('active');
+		$(".tab-content").hide();
+		i = $(this).data("block")
+		$("."+i).show();
+		return false;
+	});
+	$(".tabs a").mouseover(function(event) {
+		$(".tabs a").removeClass('hover');
+		$(this).addClass('hover');
+		return false;
+	});
+	$(".tabs a").mouseleave(function(event) {
+		$(this).removeClass('hover');
+		return false;
+	});
+	/*-------tabs-end-------*/
+
+
+
 });
